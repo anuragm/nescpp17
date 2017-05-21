2fafba8bfe0890652f7c8e2ff4ed4169bf0df83f
Anurag Mishra <anuragmi@usc.edu>
2017-05-20 20:35:51 -0700

    Improve CPU implementation
    Instead of a function to find mask, we now use a constant expression
    to compute it. This serves the same function as using enum, but a
    c++11 way to do it.
    
    Additionally, added memory to CPU/PPU.

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -2,28 +2,17 @@
 #define CPU_HPP
 
 #include "util.hpp"
+#include <cstdint>
 #include <cstddef>
 
 //Emulate a 6502 CPU.
-
-
-//A structure to access individual bits.
-template<std::size_t bitnum,typename T=u8>
-struct bitReg{
-  T data;
-
-  T    mask()  { return 1u << bitnum; }
-  void set()   { data = (data | mask()) ; }
-  void unset() { data = (data & ~mask()); }
-  T    get()   { return (data & mask())>>bitnum; }
-};
 
 //The status register. It is a 8 bit register, where each bit represents a status.
 union statReg{
   bitReg<0> C ; //Carry Flag
   bitReg<1> Z ; //Zero Flag
   bitReg<2> I ; //Interrupt enable/disable flag
-  bitReg<3> D ; //Decimal mode flag
+  bitReg<3> D ; //Decimal mode flag. Not working on NES.
   bitReg<4> B ; //Set when software interrupt is execulted.
   bitReg<5> UNUSED ;
   bitReg<6> V ; //Overflow flag
@@ -38,6 +27,12 @@ struct cpu_6502 {
   u8 Y; // Y
   u8 SP; //Stack pointer.
   u8 PC; //Program counter.
+  u8 mem[64*1024]; // The memeory of the machine. Much of this is mirrored. 64 KB
+};
+
+struct ppu_2c02{
+  u8 mem[16*1024]; //PPU memory. 16 KB Physical, 64 KB virtual
+  u8 spr_ram[256]; //Sprite RAM. 256 Bytes.
 };
 
 #endif /* CPU_HPP */
--- include/util.hpp
+++ include/util.hpp
@@ -2,10 +2,23 @@
 #define UTIL_HPP
 
 #include <cstdint>
+#include <cstddef>
+
 // Defines varies typedef and like.
 typedef uint_fast8_t  u8;
 typedef uint_fast16_t u16;
 typedef uint_fast32_t u32;
 typedef uint_fast64_t u64;
+
+//A structure to access individual bits.
+template<std::size_t bitnum,typename T=u8>
+struct bitReg{
+  T data;
+
+  constexpr const T mask() const { return 1u << bitnum; }
+  void set()   { data = (data | mask()) ; }
+  void unset() { data = (data & ~mask()); }
+  T    get()   { return (data & mask())>>bitnum; }
+};
 
 #endif /* UTIL_HPP */
