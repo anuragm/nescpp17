ee25b9637f650f40503fce1fa7d0aa4b20f66d81
Anurag Mishra <anuragmi@usc.edu>
2017-05-31 13:53:24 -0700

    Change to exact size for types
    On different platforms, the fast size may be larger than thought of
    size. For example, if 16 bit corresponds to a 64 bit variable, then
    program counter might not overflow properly. Thus, we need to change
    the define types, and let the program fail to compile if required
    types are not present.

--- include/util.hpp
+++ include/util.hpp
@@ -5,14 +5,15 @@
 #include <cstddef>
 
 // Defines varies typedef and like.
-typedef uint_fast8_t  u8;
-typedef uint_fast16_t u16;
-typedef uint_fast32_t u32;
-typedef uint_fast64_t u64;
-typedef int_fast8_t  i8;
-typedef int_fast16_t i16;
-typedef int_fast32_t i32;
-typedef int_fast64_t i64;
+typedef  uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef  int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
 
 //A structure to access individual bits.
 template<std::size_t bitnum,typename T=u8>
