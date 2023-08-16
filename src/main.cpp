b80303f3d3baaabc63ba319e9f73ec8218569475
Anurag Mishra <anurag@mishra.xyz>
2023-08-15 21:59:22 -0700

    Update main to read cartridge

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "cpu.hpp"
+#include "cartridge.hpp"
 
 int main(int argc, char** argv){
-  std::cout << "Hello World!\n";
+
+  if (argc!=2){
+    std::printf("Need a file name. %s <filename>\n",argv[0]);
+    return 0;
+  }
+
+  std::string fileName = argv[1];
+  cartridge car(fileName);
+  car.print_debug_info();
+
   return 0;
 }
